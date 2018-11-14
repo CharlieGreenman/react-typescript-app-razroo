9a0564090721c3a976c14419715b74066bd6064b
CharlieGreenman <abraham.c.greenman@gmail.com>
2018-11-14 12:58:46 -0500

    Update the file path for choose-size to be common.

---
+++ packages/common/ui/choose-size/choose-size.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class ChooseSize extends React.Component {
+  public render() {
+    return (
+      <div>Choose size works</div>
+    );
+  }
+
+}
+
+export default ;
